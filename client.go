34d0743fe6048fe65fe7f4c7221ae21bc182a998
jeseeqing <jeseeqing@gmail.com>
2024-12-12 00:16:07 +0800

    init project

---
+++ client.go
@@ -0,0 +1,54 @@
+package sdk
+
+import (
+	"fmt"
+
+	"github.com/abmpio/abmp/pkg/log"
+	pb "github.com/abmpio/hi/sdk/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	option *Options
+
+	conn *grpc.ClientConn
+
+	pb.HiClient
+	pb.CodeValueServiceClient
+}
+
+func NewClient(opts ...Option) *Client {
+	client := &Client{
+		option: newDefaultOptions(),
+	}
+	for _, eachOpt := range opts {
+		eachOpt(client.option)
+	}
+	return client
+}
+
+func (c *Client) GetOption() *Options {
+	return c.option
+}
+
+// 初始化client
+func (c *Client) InitConnnection(opts ...grpc.DialOption) error {
+	mergedOpts := make([]grpc.DialOption, 0)
+	mergedOpts = append(mergedOpts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	mergedOpts = append(mergedOpts, grpc.WithKeepaliveParams(*c.option.keepaliveParam))
+	mergedOpts = append(mergedOpts, opts...)
+	conn, err := grpc.NewClient(c.option.getHostTarget(), mergedOpts...)
+	if err != nil {
+		log.Logger.Error(fmt.Sprintf("occur error when create grpc server connection , host:%s,error: %s",
+			c.option.getHostTarget(), err.Error()))
+		return err
+	}
+	log.Logger.Info(fmt.Sprintf("initialize grpc connection finished,host:%s", c.option.getHostTarget()))
+	c.conn = conn
+	//保存客户端
+	c.HiClient = pb.NewHiClient(conn)
+	c.CodeValueServiceClient = pb.NewCodeValueServiceClient(conn)
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/abmpio/hi/sdk
+
+go 1.22.8
+
+require (
+	github.com/abmpio/abmp v0.0.0-20240708094807-9bfd8036f0dc
+	google.golang.org/grpc v1.68.1
+	google.golang.org/protobuf v1.35.2
+)
+
+require (
+	github.com/kataras/golog v0.1.11 // indirect
+	github.com/kataras/pio v0.0.13 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
+	github.com/lestrrat-go/strftime v1.0.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+)
