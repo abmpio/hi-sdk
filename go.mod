778f94543786d63914149ec50ac458ccf9abf99f
jeseeqing <jeseeqing@gmail.com>
2025-01-24 17:05:04 +0800

    move hi client initialize from startup action to service configure function

--- go.mod
+++ go.mod
@@ -12,14 +12,27 @@ require (
 )
 
 require (
+	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/fatih/color v1.15.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/hashicorp/consul/api v1.25.1 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.5.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v1.0.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hashicorp/serf v0.10.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kataras/golog v0.1.11 // indirect
 	github.com/kataras/pio v0.0.13 // indirect
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
 	github.com/lestrrat-go/strftime v1.0.6 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
@@ -27,6 +40,7 @@ require (
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.18.2 // indirect
--- starter/startup.go
+++ starter/startup.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/abmpio/abmp/pkg/log"
 	"github.com/abmpio/app"
+	"github.com/abmpio/app/cli"
 	"github.com/abmpio/hi/sdk"
 	"github.com/abmpio/hi/sdk/options"
 	pb "github.com/abmpio/hi/sdk/proto"
@@ -15,38 +16,37 @@ import (
 func init() {
 	fmt.Println("abmpio.hi_sdk.starter init")
 
-	app.RegisterOneStartupAction(clientIniStartupAction)
+	app.RegisterOneStartupAction(serviceConfigurator)
+	cli.ConfigureService(serviceConfigurator)
 }
 
-func clientIniStartupAction() app.IStartupAction {
-	return app.NewStartupAction(func() {
-		if app.HostApplication.SystemConfig().App.IsRunInCli {
-			return
-		}
-		opt := options.GetOptions()
-		_client := sdk.NewClient(sdk.WithHost(opt.HiServerHost), sdk.WithPort(opt.Port))
-		//测试ping
-		for {
-			err := _client.InitConnnection()
+func serviceConfigurator(wa cli.CliApplication) {
+	if app.HostApplication.SystemConfig().App.IsRunInCli {
+		return
+	}
+	opt := options.GetOptions()
+	_client := sdk.NewClient(sdk.WithHost(opt.HiServerHost), sdk.WithPort(opt.Port))
+	//测试ping
+	for {
+		err := _client.InitConnnection()
+		if err == nil {
+			var res *pb.HealthCheckResponse
+			res, err = _client.HealthCheck(context.TODO(), &pb.HealthCheckRequest{})
 			if err == nil {
-				var res *pb.HealthCheckResponse
-				res, err = _client.HealthCheck(context.TODO(), &pb.HealthCheckRequest{})
-				if err == nil {
-					break
-				}
-				if res != nil {
-					log.Logger.Info(res.Status.String())
-				}
+				break
 			}
-
-			log.Logger.Warn(err.Error())
-			log.Logger.Warn("2s后重新测试...")
-			time.Sleep(2 * time.Second)
+			if res != nil {
+				log.Logger.Info(res.Status.String())
+			}
 		}
-		sdk.SetGlobalClient(_client)
-		app.Context.RegistInstanceAs(_client, new(pb.HiClient))
-		app.Context.RegistInstanceAs(_client, new(pb.CodeValueServiceClient))
-		app.Context.RegistInstanceAs(_client, new(pb.SettingsServiceClient))
-		app.Context.RegistInstanceAs(_client, new(sdk.Client))
-	})
+
+		log.Logger.Warn(err.Error())
+		log.Logger.Warn("2s后重新测试...")
+		time.Sleep(2 * time.Second)
+	}
+	sdk.SetGlobalClient(_client)
+	app.Context.RegistInstanceAs(_client, new(pb.HiClient))
+	app.Context.RegistInstanceAs(_client, new(pb.CodeValueServiceClient))
+	app.Context.RegistInstanceAs(_client, new(pb.SettingsServiceClient))
+	app.Context.RegistInstanceAs(_client, new(sdk.Client))
 }
